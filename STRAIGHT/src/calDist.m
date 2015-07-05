7157b3837b241d510cfae58880e2ed501971f273
柯均洁 <kjj3300@163.com>
2015-07-05 17:48:59 -0400

    fin

--- STRAIGHT/src/calDist.m
+++ STRAIGHT/src/calDist.m
@@ -1,99 +1,96 @@
 clear;
 clc;
-avgF0Dist('../data/exp2/B/sen6147.wav', '../data/exp2/A/sen6147_2.wav');
-avgAmplitudeDist('../data/exp2/B/sen6147.wav', '../data/exp2/A/sen6147_2.wav');
-avgFrequencyDist('../data/exp2/B/sen6147.wav', '../data/exp2/A/sen6147_2.wav');
 
-% fprintf('-------- avgF0Dist ---------\n');
-% fprintf('guodegang: \n');
-% avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
-% avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
-% avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
+fprintf('-------- avgF0Dist ---------\n');
+fprintf('guodegang: \n');
+avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
+avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
+avgF0Dist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
 
-% fprintf('shantianfang: \n');
-% avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
-% avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
-% avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
+fprintf('shantianfang: \n');
+avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
+avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
+avgF0Dist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
 
-% fprintf('sen6000: \n');
-% avgF0Dist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
-% avgF0Dist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
+fprintf('sen6000: \n');
+avgF0Dist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
+avgF0Dist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
 
-% fprintf('sen6015: \n');
-% avgF0Dist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
-% avgF0Dist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
+fprintf('sen6015: \n');
+avgF0Dist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
+avgF0Dist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
 
-% fprintf('sen6028: \n');
-% avgF0Dist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
-% avgF0Dist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
+fprintf('sen6028: \n');
+avgF0Dist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
+avgF0Dist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
 
-% fprintf('sen6044: \n');
-% avgF0Dist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
-% avgF0Dist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
+fprintf('sen6044: \n');
+avgF0Dist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
+avgF0Dist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
 
-% fprintf('sen6147: \n');
-% avgF0Dist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
-% avgF0Dist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
+fprintf('sen6147: \n');
+avgF0Dist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
+avgF0Dist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
 
-% fprintf('-------- avgAmplitudeDist ---------\n');
-% fprintf('guodegang: \n');
-% avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
-% avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
-% avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
+fprintf('-------- avgAmplitudeDist ---------\n');
+fprintf('guodegang: \n');
+avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
+avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
+avgAmplitudeDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
 
-% fprintf('shantianfang: \n');
-% avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
-% avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
-% avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
+fprintf('shantianfang: \n');
+avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
+avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
+avgAmplitudeDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
 
-% fprintf('sen6000: \n');
-% avgAmplitudeDist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
-% avgAmplitudeDist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
+fprintf('sen6000: \n');
+avgAmplitudeDist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
+avgAmplitudeDist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
 
-% fprintf('sen6015: \n');
-% avgAmplitudeDist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
-% avgAmplitudeDist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
+fprintf('sen6015: \n');
+avgAmplitudeDist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
+avgAmplitudeDist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
 
-% fprintf('sen6028: \n');
-% avgAmplitudeDist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
-% avgAmplitudeDist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
+fprintf('sen6028: \n');
+avgAmplitudeDist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
+avgAmplitudeDist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
 
-% fprintf('sen6044: \n');
-% avgAmplitudeDist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
-% avgAmplitudeDist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
+fprintf('sen6044: \n');
+avgAmplitudeDist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
+avgAmplitudeDist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
 
-% fprintf('sen6147: \n');
-% avgAmplitudeDist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
-% avgAmplitudeDist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
+fprintf('sen6147: \n');
+avgAmplitudeDist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
+avgAmplitudeDist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
 
-% fprintf('-------- avgFrequencyDist ---------\n');
-% fprintf('guodegang: \n');
-% avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
-% avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
-% avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
+fprintf('-------- avgFrequencyDist ---------\n');
+fprintf('guodegang: \n');
+avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_1.wav');
+avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_2.wav');
+avgFrequencyDist('../data/exp1/guodegang.wav', '../res_data/exp1/guodegang_3.wav');
 
-% fprintf('shantianfang: \n');
-% avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
-% avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
-% avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
+fprintf('shantianfang: \n');
+avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_1.wav');
+avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_2.wav');
+avgFrequencyDist('../data/exp1/shantianfang.wav', '../res_data/exp1/shantianfang_3.wav');
 
-% fprintf('sen6000: \n');
-% avgFrequencyDist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
-% avgFrequencyDist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
+fprintf('sen6000: \n');
+avgFrequencyDist('../data/exp2/A/sen6000.wav', '../data/exp2/B/sen6000.wav');
+avgFrequencyDist('../data/exp2/B/sen6000.wav', '../res_data/exp2/sen6000_2.wav');
 
-% fprintf('sen6015: \n');
-% avgFrequencyDist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
-% avgFrequencyDist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
+fprintf('sen6015: \n');
+avgFrequencyDist('../data/exp2/A/sen6015.wav', '../data/exp2/B/sen6015.wav');
+avgFrequencyDist('../data/exp2/B/sen6015.wav', '../res_data/exp2/sen6015_2.wav');
 
-% fprintf('sen6028: \n');
-% avgFrequencyDist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
-% avgFrequencyDist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
+fprintf('sen6028: \n');
+avgFrequencyDist('../data/exp2/A/sen6028.wav', '../data/exp2/B/sen6028.wav');
+avgFrequencyDist('../data/exp2/B/sen6028.wav', '../res_data/exp2/sen6028_2.wav');
 
-% fprintf('sen6044: \n');
-% avgFrequencyDist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
-% avgFrequencyDist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
+fprintf('sen6044: \n');
+avgFrequencyDist('../data/exp2/A/sen6044.wav', '../data/exp2/B/sen6044.wav');
+avgFrequencyDist('../data/exp2/B/sen6044.wav', '../res_data/exp2/sen6044_2.wav');
 
-% fprintf('sen6147: \n');
-% avgFrequencyDist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
-% avgFrequencyDist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
+fprintf('sen6147: \n');
+avgFrequencyDist('../data/exp2/A/sen6147.wav', '../data/exp2/B/sen6147.wav');
+avgFrequencyDist('../data/exp2/B/sen6147.wav', '../res_data/exp2/sen6147_2.wav');
 
